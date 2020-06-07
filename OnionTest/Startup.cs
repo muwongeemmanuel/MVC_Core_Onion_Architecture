d6a689905c491ac31df0d4a740f40716559a8bac
Emmanuel Muwonge <muwongeemmanuel123@gmail.com>
2020-06-07 09:28:16 +0300

    Final commit before zipping

---
+++ OnionTest/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace OnionTest
+{
+    [SetUpFixture]
+    public class Startup
+    {
+        internal static IServiceProvider ServiceProvider { get; set; }
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+            ServiceProvider = ContainerBuilder();
+        }
+        [OneTimeTearDown]
+        public void RunAfterAnyTests()
+        {
+
+        }
+        public IServiceProvider ContainerBuilder()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.ConfigureIOC();
+            return services.BuildServiceProvider();
+        }
+    }
+}
